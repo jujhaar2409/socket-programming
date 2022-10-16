71da3cea5223b7f190251d7d41277e9120792405
rohankalbag <kalbag.rohan.r@gmail.com>
2022-10-16 21:02:53 +0530

    Added udp, interrupts not working

--- client.cpp
+++ client.cpp
@@ -1,8 +1,7 @@
 // code for socket binding taken from https://www.geeksforgeeks.org/socket-programming-cc/ 
 
-// Client side C/C++ program to demonstrate Socket
-// programming
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -13,16 +12,21 @@
 #include <unistd.h>
 #include <signal.h>
 #include <regex>
-#define PORT 8080
+#include <chrono>
 
+using std::chrono::system_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 using namespace std;
 // regex ack_regex("Acknowledgement:[0-9]+");
 // regex ack_regex(":*");
 regex ack_regex("[\\S|\\s]+(Acknowledgement:[0-9]+)[\\s|\\S]+");
 
-#define gettime ((std::chrono::system_clock::now() - start).count()) 
+#define gettime duration_cast<milliseconds>(system_clock::now() - start).count()
 #define cout2 cout << gettime << " - "
 
+struct sockaddr_in serv_address;
 int seq_num_send;
 int PORT_S;
 int PORT_R;
@@ -30,12 +34,14 @@ int RETRANSMIT_TIME;
 int NUM_PACKETS;
 bool got_ack = true;
 string packet;
-int sock;
+int client_fd;
+std::chrono::_V2::system_clock::time_point start;
 
 //interrupt service routine
 void timeout(int sig)
 {
-	send(sock, packet.c_str(), packet.length(), 0); 
+	sendto(client_fd, packet.c_str(), packet.length(), MSG_CONFIRM, (const struct sockaddr *) &serv_address, sizeof(serv_address)); 
+	cout2 << "isr send: " << packet.c_str() << endl;
 	signal(SIGALRM, &timeout);  // set a signal handler
 	ualarm(RETRANSMIT_TIME * 1e3, 0);
 }
@@ -48,49 +54,47 @@ int get_seq_num(string packet) {
 
 int main(int argc, char const* argv[])
 {
-	auto start = std::chrono::system_clock::now();
+	start = std::chrono::system_clock::now();
 
 	PORT_S = stoi(argv[1]);
 	PORT_R = stoi(argv[2]);
-	RETRANSMIT_TIME = stoi(argv[3]);
+	RETRANSMIT_TIME = stof(argv[3])*1000;
 	NUM_PACKETS = stoi(argv[4]);
 
 	//generate packets to transmit
 	vector<string> packets;
+	
 	for(int i = 1; i <= NUM_PACKETS; i++){
 		string pkt = "Packet:" + to_string(i);
 		packets.push_back(pkt);
 	}
 	
-	sock = 0; 
-	int valread, client_fd;
-    struct sockaddr_in serv_addr;
+	int valread;
     char buffer[1024] = { 0 };
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
+    if ((client_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        printf("\n socket creation failure \n");
         return -1;
     }
- 
-    serv_addr.sin_family = AF_INET;
-    // serv_addr.sin_port = htons(PORT);
-    serv_addr.sin_port = htons(PORT);
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
- 
-    if((client_fd = connect(sock, (struct sockaddr*)&serv_addr,sizeof(serv_addr))) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
+	
+	memset(&serv_address, 0, sizeof(serv_address));
 
+    serv_address.sin_family = AF_INET;
+    serv_address.sin_port = htons(PORT_R);
+	serv_address.sin_addr.s_addr = INADDR_ANY;
+
+	socklen_t length;
+	
 	seq_num_send = 1;
 	while(seq_num_send <= NUM_PACKETS){
 		packet = packets[seq_num_send - 1];
 		if(got_ack){	
-			send(sock, packet.c_str(), packet.length(), 0); 
+			sendto(client_fd, packet.c_str(), packet.length(), MSG_CONFIRM, (const struct sockaddr *) &serv_address, sizeof(serv_address)); 
 			cout2 << "send: " << packet.c_str() << endl;
 			signal(SIGALRM, &timeout);  // set a signal handler
 			ualarm(RETRANSMIT_TIME * 1e3, 0);
 		}
-		valread = read(sock, buffer, 1024);
+		valread = recvfrom(client_fd, (char *) buffer, 1024, MSG_WAITALL, (struct sockaddr *) &serv_address, &length);
+		buffer[valread] = '\0';
 		cout2 << "recv: " << buffer << endl;
 		// if(regex_match(buffer, ack_regex)) {
 		if(buffer[0] == 'A') {
--- server.cpp
+++ server.cpp
@@ -1,8 +1,7 @@
 // code for socket binding taken from https://www.geeksforgeeks.org/socket-programming-cc/ 
 
-// Server side C/C++ program to demonstrate Socket
-// programming
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,21 +12,24 @@
 #include <random>
 #include <regex>
 #include <chrono>
-#define PORT 8080
 
+using std::chrono::system_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 using namespace std;
 
-// regex pkt_regex("Packet:[0-9]+");
 regex pkt_regex("[\\S|\\s]+(Packet:[0-9]+)[\\s|\\S]+");
 
-#define gettime ((std::chrono::system_clock::now() - start).count()) 
+#define gettime duration_cast<milliseconds>(system_clock::now() - start).count()
 #define cout2 cout << gettime << " - "
 
 int seq_num_expect = 1;
+
 string generate_next_acknowledgement(int seq_num)
 {
 	// seq_num += 1; 
-	return "Acknowledgement:" + to_string(seq_num);
+	return "Acknowledgement:" + to_string(2);
 }
 
 int get_seq_num(string packet) {
@@ -38,64 +40,42 @@ int get_seq_num(string packet) {
 
 int main(int argc, char const* argv[])
 {
-	auto start = std::chrono::system_clock::now();
-	// for (int i = 0; i < argc; i++) {
-	// 	cout2 << argv[i] << endl;
-	// }
+	auto start = system_clock::now();
 
 	int PORT_R = stoi(argv[1]);
 	int PORT_S = stoi(argv[2]);
 	float DROP_PROB = stof(argv[3]);
 
-	int server_fd, new_socket, valread;
-	struct sockaddr_in address;
-	int opt = 1;
-	int addrlen = sizeof(address);
+	int server_fd, new_socket;
+	struct sockaddr_in serv_address, cli_address;
 	char buffer[1024] = { 0 };
-	char* hello = "Hello from server";
 
 	// Creating socket file descriptor
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("socket failed");
+	if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
 
-	// Forcefully attaching socket to the port 8080
-	// if (setsockopt(server_fd, SOL_SOCKET,
-	// 			SO_REUSEADDR | SO_REUSEPORT, &opt,
-	// 			sizeof(opt))) {
-	if (setsockopt(server_fd, SOL_SOCKET,
-				SO_DEBUG, &opt,
-				sizeof(opt))) {
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(PORT);
+	memset(&serv_address, 0, sizeof(serv_address));
+	memset(&cli_address, 0, sizeof(cli_address));
 
-	// Forcefully attaching socket to the port 8080
-	if (bind(server_fd, (struct sockaddr*)&address,
-             sizeof(address)) < 0) {
+
+	serv_address.sin_family = AF_INET;
+	serv_address.sin_addr.s_addr = INADDR_ANY;
+	serv_address.sin_port = htons(PORT_R);
+
+	if (bind(server_fd, (struct sockaddr*)&serv_address,
+             sizeof(serv_address)) < 0) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-	if (listen(server_fd, 3) < 0) {
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
-
-	if ((new_socket
-			= accept(server_fd, (struct sockaddr*)&address,
-					(socklen_t*)&addrlen))
-			< 0) {
-			perror("accept");
-			exit(EXIT_FAILURE);
-		}
-
-
+	
+	int valread;
+	socklen_t length = sizeof(cli_address);
+	
 	while (true) {
-		valread = read(new_socket, buffer, 1024);
+		valread = recvfrom(server_fd, (char *) buffer, 1024, MSG_WAITALL, (struct sockaddr *) &cli_address, &length);
+		buffer[valread] ='\0';
 		cout2 << "recv: " << buffer << endl;
 		// if (regex_match(buffer, pkt_regex)) {
 		if (buffer[0] == 'P') {
@@ -114,17 +94,10 @@ int main(int argc, char const* argv[])
 				cout2 << "<< send ack for CURRENT packet >>" << endl;
 			}
 			string acknowledgement = generate_next_acknowledgement(seq_num_expect);
-			send(new_socket, acknowledgement.c_str(), acknowledgement.length(), 0);
+			sendto(server_fd, acknowledgement.c_str(), acknowledgement.length(), MSG_CONFIRM, (const struct sockaddr *) &cli_address, length);
 			cout2 << "send: " << acknowledgement.c_str() << endl;
 		}
-		// printf("%s\n", buffer);
-		// send(new_socket, hello, strlen(hello), 0);
-		// printf("Hello message sent\n");
 	}
-
-	// closing the connected socket
-	close(new_socket);
-	// closing the listening socket
 	shutdown(server_fd, SHUT_RDWR);
 	return 0;
 }
